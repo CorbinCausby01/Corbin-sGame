7e69c99e2a5b811a8b80e91085166c6938e15ec4
CorbinCausby01 <Dirt7873@gmail.com>
2023-03-14 19:24:06 -0400

    Choices

---
+++ My project/Assets/DialogueFolder/PokeTypeSelection.ink
@@ -0,0 +1,17 @@
+-> main
+
+=== main ===
+Which type of Poke do you choose?
+    +[Streamer]
+        -> chosen("Streamer")
+    +[Basketball Player]
+        -> chosen("Basketball Player")
+    +[Rapper]
+        -> chosen("Rapper")
+    +[Soccer Player]
+        ->chosen("Soccer Player")
+    
+=== chosen(Poke) ===
+you chose {Poke}!
+->END
+    
--- My project/Assets/DialogueManger.cs
+++ My project/Assets/DialogueManger.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Ink.Runtime;
+using UnityEngine.EventSystems;
 
 
 
@@ -19,6 +20,10 @@ namespace DialogueManager
         [SerializeField] private GameObject dialoguePanel;
         [SerializeField] public TextMeshProUGUI dialogueText;
 
+        [Header("Choices UI")]
+        [SerializeField] private GameObject[] choices;
+        private TextMeshProUGUI[] choicesText;
+
         private Story currentStory;
 
         public TextMeshProUGUI DialogueName;
@@ -30,6 +35,14 @@ namespace DialogueManager
         private void Start()
         {
             instance = this;
+
+            choicesText = new TextMeshProUGUI[choices.Length];
+            int index = 0;
+            foreach (GameObject choice in choices)
+            {
+                choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+                index++;
+            }
         }
 
         //private void Update()
@@ -52,8 +65,6 @@ namespace DialogueManager
         public void StartDialogue(TextAsset inkJSON)
         {
 
-            Debug.Log(inkJSON.text);
-
             currentStory = new Story(inkJSON.text);
 
             animator.SetBool("IsOpen", true);
@@ -66,7 +77,11 @@ namespace DialogueManager
         {
             if(currentStory.canContinue)
             {
+                // set text for the current dialogue line
                 dialogueText.text = currentStory.Continue();
+                // display choices, if any, for this dialogue line
+                DisplayChoices();
+
             }
             else
             {
@@ -91,6 +106,50 @@ namespace DialogueManager
         {
             animator.SetBool("IsOpen", false);
         }
+
+        public void DisplayChoices()
+        {
+            List<Choice> currentChoices = currentStory.currentChoices;
+            
+
+            //Check to make sure our UI can support the number of choices in.
+            if (currentChoices.Count > choices.Length)
+            {
+                Debug.LogError("More choices were given than the UI can support. Number of choices given: " + currentChoices.Count);
+            }
+
+            int index = 0;
+            // enable and initialize the choices up to the amount of the choices for this line of dialogue
+            foreach (Choice choice in currentChoices)
+            {
+                choices[index].gameObject.SetActive(true);
+                choicesText[index].text = choice.text;
+                index++;
+            }
+            // go through the remaining choices the Ui supports and make sure theyre hidden
+            for (int i = index; i < choices.Length; i++)
+            {
+                choices[i].gameObject.SetActive(false);
+            }
+
+            StartCoroutine(SelectFirstChoice());
+
+            
+        }
+
+        public IEnumerator SelectFirstChoice()
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            yield return new WaitForEndOfFrame();
+            EventSystem.current.SetSelectedGameObject(choices[0].gameObject);
+        }
+
+        public void MakeChoice(int choiceIndex)
+        {
+            currentStory.ChooseChoiceIndex(choiceIndex);
+        }
+
+
     }
 }
 
